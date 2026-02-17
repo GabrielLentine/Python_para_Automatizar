96fb7401b27b7da3995a02a2d601ffa03086ac54
Gabriel Lentine <gabrielmarinholentine@gmail.com>
2026-02-17 17:39:51 -0300

    feat: aula sobre bibliotecas internas e externas

---
+++ 10_Bibliotecas/bibliotecas_externas.py
@@ -0,0 +1,5 @@
+# pip install <nome_da_biblioteca>
+import pyfiglet
+
+texto = pyfiglet.figlet_format("Gabriel Lentine") # letreiro
+print(texto)
---
+++ 10_Bibliotecas/bibliotecas_padrao.py
@@ -0,0 +1,11 @@
+import math # biblioteca criada p/ lidar com funções matemáticas
+import random # biblioteca criada p/ lidar com aleatoriedade
+
+# math
+print(math.sqrt(49)) # raiz quadrada de um número
+print(math.ceil(4.55)) # arredondamento de um número
+print(math.pi) # 3,14...
+
+# random
+numero_aleatorio = random.randint(1, 10) # número aleatório entre 1 e 10
+print(numero_aleatorio)
